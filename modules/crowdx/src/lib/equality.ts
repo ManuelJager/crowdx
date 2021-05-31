581ef55c78f5aed9c19cc4d3dfa3a81cc38a939c
stijnstroeve <stijnstroeve@hotmail.com>
2021-06-01 00:28:36 +0200

    Added noequality..

--- modules/crowdx/src/lib/equality.ts
+++ modules/crowdx/src/lib/equality.ts
@@ -3,5 +3,6 @@ export type EqualityLambda<ValueT> = (newValue: ValueT, oldValue: ValueT) => boo
 
 export const DoubleEquals: EqualityLambda<any> = (newValue: any, oldValue: any) => newValue == oldValue;
 export const TripleEquals: EqualityLambda<any> = (newValue: any, oldValue: any) => newValue === oldValue;
+export const NoEquality: EqualityLambda<any> = () => false;
 
 export const DefaultEqualityLambda: EqualityLambda<any> = TripleEquals;
