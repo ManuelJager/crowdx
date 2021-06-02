275dd4f8d12e3c414afd07bbd82733fc62f7c8cc
stijnstroeve <stijnstroeve@hotmail.com>
2021-06-02 23:09:26 +0200

    Added when tests.

--- modules/crowdx/src/api/when.ts
+++ modules/crowdx/src/api/when.ts
@@ -1,13 +1,13 @@
-import { Observable } from './observable';
-import { observe } from './index';
+import { Observable } from './observable'
+import { observe } from './index'
 
-type WhenCondition<ValueT> = (value: ValueT) => boolean;
+type WhenCondition<ValueT> = (value: ValueT) => boolean
 
-const when = <ValueT>(num: Observable<ValueT>, condition: WhenCondition<ValueT>): Promise<ValueT> => {
-  return new Promise<ValueT>((resolve) => {
+const when = async <ValueT>(num: Observable<ValueT>, condition: WhenCondition<ValueT>): Promise<ValueT> => {
+  return await new Promise<ValueT>((resolve) => {
     observe(num, (val) => {
-      if(condition(val as ValueT)) {
-        resolve(val as ValueT);
+      if (condition(val as ValueT)) {
+        resolve(val as ValueT)
       }
     })
   })
